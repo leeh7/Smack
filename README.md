841d9c220596a1aebbfc1132bda629b162ea7241
David Lee <leed13@wit.edu>
2017-11-11 17:34:25 -0500

    Implemented working chat message timestamps and created readme file (in progress)

---
+++ README.md
@@ -0,0 +1,10 @@
+# Smack iOS App
+
+## Summary
+- Slack-style Chat Messaging iOS App written primarily in Swift 
+- Independent personal project worked on as part of Devslope's iOS11 App Developer Course on Udemy
+
+## Skills & Concepts Learned
+-     
+
+
--- Smack/View/MessageCell.swift
+++ Smack/View/MessageCell.swift
@@ -25,5 +25,21 @@ class MessageCell: UITableViewCell {
         userNameLbl.text = message.userName
         userImg.image = UIImage(named: message.userAvatar)
         userImg.backgroundColor = UserDataService.instance.returnUIColor(components: message.userAvatarColor)
+        
+        //Timestamp parsing
+        guard var isoDate = message.timeStamp else { return }
+        let end = isoDate.index(isoDate.endIndex, offsetBy: -5)
+        isoDate = String(isoDate[..<end])
+        
+        let isoFormatter = ISO8601DateFormatter()
+        let chatDate = isoFormatter.date(from: isoDate.appending("Z"))
+        
+        let newFormatter = DateFormatter()
+        newFormatter.dateFormat = "MMM d, h:mm a"
+        
+        if let finalDate = chatDate {
+            let finalDate = newFormatter.string(from: finalDate)
+            timeStampLbl.text = finalDate
+        }
     }
 }
